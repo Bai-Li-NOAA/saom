77b6ebc5593ce4559c6c10835508c0df4af21e3a
Bai Li <bai.li@noaa.gov>
2020-07-28 14:30:18 -0400

    remove R6 code

--- R/growth.R
+++ R/growth.R
@@ -1,54 +1,54 @@
-library("R6")
-
-Growth <- R6Class("Growth",
-  public = list(
-    length_at_age = NULL,
-    weight_at_age = NULL
-  )
-)
-
-vonBertalanffy <- R6Class("vonBertalanffy",
-  inherit = Growth,
-  public = list(
-    linf = NULL,
-    k = NULL,
-    a0 = NULL,
-
-    initialize = function(linf, k, a0, age_vector, nyears) {
-      self$linf <- linf
-      self$k <- k
-      self$a0 <- a0
-      self$length_at_age <- matrix(
-        rep(linf*(1-exp(-k*(age_vector-a0))), times=nyears),
-        nrow=nyears,
-        byrow=TRUE)
-    }
-  )
-)
-
-PowerWeight <- R6Class("PowerWeight",
-  inherit = Growth,
-  public = list(
-    coefficient = NULL,
-    exponent = NULL,
-    power_function = power,
-
-    initialize = function(coefficient, exponent, length_at_age) {
-      self$coefficient <- coefficient
-      self$exponent <- exponent
-      self$weight_at_age <- self$power_function(length_at_age, coefficient, exponent)
-    }
-  )
-)
-
-GrowthFactory <- R6Class("GrowthFactory",
-  public = list(
-    vonBertalanffy = function(linf, k, a0, age_vector, nyears) {
-      vonBertalanffy$new(linf, k, a0, age_vector, nyears)
-    },
-    PowerWeight = function(coefficient, exponent, length_at_age) {
-      PowerWeight$new(coefficient, exponent, length_at_age)
-    }
-  )
-)
-
+# library("R6")
+#
+# Growth <- R6Class("Growth",
+#   public = list(
+#     length_at_age = NULL,
+#     weight_at_age = NULL
+#   )
+# )
+#
+# vonBertalanffy <- R6Class("vonBertalanffy",
+#   inherit = Growth,
+#   public = list(
+#     linf = NULL,
+#     k = NULL,
+#     a0 = NULL,
+#
+#     initialize = function(linf, k, a0, age_vector, nyears) {
+#       self$linf <- linf
+#       self$k <- k
+#       self$a0 <- a0
+#       self$length_at_age <- matrix(
+#         rep(linf*(1-exp(-k*(age_vector-a0))), times=nyears),
+#         nrow=nyears,
+#         byrow=TRUE)
+#     }
+#   )
+# )
+#
+# PowerWeight <- R6Class("PowerWeight",
+#   inherit = Growth,
+#   public = list(
+#     coefficient = NULL,
+#     exponent = NULL,
+#     power_function = power,
+#
+#     initialize = function(coefficient, exponent, length_at_age) {
+#       self$coefficient <- coefficient
+#       self$exponent <- exponent
+#       self$weight_at_age <- self$power_function(length_at_age, coefficient, exponent)
+#     }
+#   )
+# )
+#
+# GrowthFactory <- R6Class("GrowthFactory",
+#   public = list(
+#     vonBertalanffy = function(linf, k, a0, age_vector, nyears) {
+#       vonBertalanffy$new(linf, k, a0, age_vector, nyears)
+#     },
+#     PowerWeight = function(coefficient, exponent, length_at_age) {
+#       PowerWeight$new(coefficient, exponent, length_at_age)
+#     }
+#   )
+# )
+#
--- R/maturity.R
+++ R/maturity.R
@@ -1,36 +1,36 @@
-#install.packages("R6")
-
-library("R6")
-
-Maturity <- R6Class("Maturity",
-  public = list(
-    maturity_at_age = NULL
-  )
-)
-
-SimpleLogisticMaturity <- R6Class("SimpleLogisticMaturity",
-  inherit = Maturity,
-  public = list(
-    slope = NULL,
-    age_50 = NULL,
-    simple_logistic_function = logistic,
-
-    initialize = function(slope, age_50, age_vector, nyears) {
-      self$slope <- slope
-      self$age_50 <- age_50
-      self$maturity_at_age <- matrix(
-        rep(self$simple_logistic_function(age_vector, slope, age_50), times=nyears),
-        nrow=nyears,
-        byrow=TRUE)
-    }
-  )
-)
-
-MaturityFactory <- R6Class("MaturityFactory",
-  public = list(
-    SimpleLogisticMaturity = function(slope, age_50, age_vector, nyears) {
-      SimpleLogisticMaturity$new(slope, age_50, age_vector, nyears)
-    }
-  )
-)
-
+# #install.packages("R6")
+#
+# library("R6")
+#
+# Maturity <- R6Class("Maturity",
+#   public = list(
+#     maturity_at_age = NULL
+#   )
+# )
+#
+# SimpleLogisticMaturity <- R6Class("SimpleLogisticMaturity",
+#   inherit = Maturity,
+#   public = list(
+#     slope = NULL,
+#     age_50 = NULL,
+#     simple_logistic_function = logistic,
+#
+#     initialize = function(slope, age_50, age_vector, nyears) {
+#       self$slope <- slope
+#       self$age_50 <- age_50
+#       self$maturity_at_age <- matrix(
+#         rep(self$simple_logistic_function(age_vector, slope, age_50), times=nyears),
+#         nrow=nyears,
+#         byrow=TRUE)
+#     }
+#   )
+# )
+#
+# MaturityFactory <- R6Class("MaturityFactory",
+#   public = list(
+#     SimpleLogisticMaturity = function(slope, age_50, age_vector, nyears) {
+#       SimpleLogisticMaturity$new(slope, age_50, age_vector, nyears)
+#     }
+#   )
+# )
+#
